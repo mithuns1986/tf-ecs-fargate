f14bd99b410aee842db73da4fc0c302488957b28
Mithun <mithun.s@sgtradex.com>
2022-11-28 11:59:03 +0530

    added second ecs service

--- ecs.tf
+++ ecs.tf
@@ -26,8 +26,25 @@ resource "aws_ecs_task_definition" "app" {
   container_definitions    = data.template_file.myapp.rendered
 }
 
+resource "aws_ecs_task_definition" "app2" {
+  family                   = "myapp-task"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  container_definitions    = data.template_file.myapp.rendered
+}
+
 resource "aws_ecs_service" "main" {
   name            = "myapp-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.app_count
+  launch_type     = "FARGATE"
+
+resource "aws_ecs_service" "main2" {
+  name            = "myapp-service2"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = var.app_count
